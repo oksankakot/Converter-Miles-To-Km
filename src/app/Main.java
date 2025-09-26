1cc43b39a732c35351bf0897a9690fab24c61a52
Oksana Kotlyarenko <oksanka.kot@gmail.com>
2025-09-26 12:16:57 +0300

    Add miles to kms functionality

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,5 +6,17 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("App for converting miles to kilometers.");
+
+        double milesValue = 36;
+        double kilometersResult = convMilesToKilometers(milesValue);
+        System.out.println("Result of conversion of " + milesValue +  " miles is " + kilometersResult + " kilometers.");
     }
+
+    private static double convMilesToKilometers(double mls) {
+        return mls * CONV_K;
+    }
+
+
+
+
 }
