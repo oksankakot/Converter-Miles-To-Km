6b0916ed6b8d6cac9c9adee1756b6e4b9786702d
Oksana Kotlyarenko <oksanka.kot@gmail.com>
2025-09-26 12:01:19 +0300

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,10 @@
+package app;
+
+public class Main {
+
+    private static final double CONV_K = 1.60934;
+
+    public static void main(String[] args) {
+        System.out.println("App for converting miles to kilometers.");
+    }
+}
